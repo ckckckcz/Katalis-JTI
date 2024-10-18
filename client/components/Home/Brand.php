dabcfc934248c430890c12539c832a926bdb8c43
ckckckcz <rriovld@gmail.com>
2024-10-18 14:42:15 +0700

    update: brands logo

---
+++ client/components/Home/Brand.php
@@ -0,0 +1,23 @@
+<section class="bg-white dark:bg-gray-900 mt-10">
+    <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+        <div class="grid grid-cols-1 gap-4">
+            <!-- Baris 1 -->
+            <div class="flex justify-center gap-16 ">
+                <img src="/UTS/public/img/brand/nike.png" alt="Nike"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/adidas.png" alt="Adidas"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/puma.png" alt="Puma"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/underArmor.png" alt="Under Armour"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/reebok.png" alt="Reebok"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/newBalance.png" alt="New Balance"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+                <img src="/UTS/public/img/brand/asics.png" alt="Asics"
+                    class="h-12 mx-2 grayscale hover:grayscale-0 transition duration-500 ease-in-out transform hover:scale-110">
+            </div>
+        </div>
+    </div>
+</section>
