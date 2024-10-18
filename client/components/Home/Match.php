0c90cdff55296a4af01a81fa3b03eed15c770003
ckckckcz <rriovld@gmail.com>
2024-10-18 20:34:10 +0700

    feat: 3 match result

--- client/components/Home/Match.php
+++ client/components/Home/Match.php
@@ -1,6 +1,6 @@
 <section class="bg-black">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <div class="match flex justify-between">
+        <div class="match flex justify-between mb-20">
             <div class="titleMatch text-white text-left">
                 <h5 class="text-sm mb-4">Starting - Mar 23, 19.00</h5>
                 <h1 class="text-6xl mb-4">Next <br> Match</h1>
@@ -15,6 +15,69 @@
                 <div class="gamers bg-white p-16 w-64 h-64 flex justify-center items-center">
                     <img src="/UTS/public/img/gamers.png" alt="">
                 </div>
+            </div>
+        </div>
+        <div class="matchResult text-white text-left">
+            <h5 class="text-2xl">previous 3 matches</h5>
+            <div class="result">
+                <div
+                    class="matchOne justify-between text-white text-opacity-20 p-5 flex flex-col lg:flex-row items-center gap-4 text-center">
+                    <h5 class="text-2xl">L</h5>
+                    <h5 class="text-sm">Ended - Feb 14, 18:43</h5>
+                    <h5 class="text-sm">ShadowStrike Invitational</h5>
+                    <div class="team flex gap-6 items-center">
+                        <div class="gritGrind flex gap-3 items-center">
+                            <img src="/UTS/public/img/gritGrand.png" class="w-10 h-auto opacity-20" alt="">
+                            <h5 class="text-sm">Grit and Grind</h5>
+                        </div>
+                        <div class="score">
+                            <h5 class="text-sm">0 - 2</h5>
+                        </div>
+                        <div class="paperPlane flex gap-3 items-center">
+                            <h5 class="text-sm">paperplane</h5>
+                            <img src="/UTS/public/img/paperPlane.png" class="w-10 h-auto opacity-20" alt="">
+                        </div>
+                    </div>
+                </div>
+                <hr class="border-[#2D2D2D]">
+                <div class="matchOne justify-between p-5 flex flex-col lg:flex-row items-center gap-4 text-center">
+                    <h5 class="text-2xl">W</h5>
+                    <h5 class="text-sm">Ended - Feb 13, 17:14</h5>
+                    <h5 class="text-sm">ShadowStrike Invitational</h5>
+                    <div class="team flex gap-6 items-center">
+                        <div class="denied flex gap-3 items-center">
+                            <h5 class="text-sm">Denied</h5>
+                            <img src="/UTS/public/img/denied.png" class="w-10 h-auto" alt="">
+                        </div>
+                        <div class="score">
+                            <h5 class="text-sm">0 - 1</h5>
+                        </div>
+                        <div class="gritGrind flex gap-3 items-center">
+                            <img src="/UTS/public/img/gritGrand.png" class="w-10 h-auto" alt="">
+                            <h5 class="text-sm">Grit and Grind</h5>
+                        </div>
+                    </div>
+                </div>
+                <hr class="border-[#2D2D2D]">
+                <div class="matchOne justify-between p-5 flex flex-col lg:flex-row items-center gap-4 text-center">
+                    <h5 class="text-2xl">W</h5>
+                    <h5 class="text-sm">Ended - Feb 13, 13:51</h5>
+                    <h5 class="text-sm">ShadowStrike Invitational</h5>
+                    <div class="team flex gap-6 items-center">
+                        <div class="gritGrind flex gap-3 items-center">
+                            <img src="/UTS/public/img/gritGrand.png" class="w-10 h-auto" alt="">
+                            <h5 class="text-sm">Grit and Grind</h5>
+                        </div>
+                        <div class="score">
+                            <h5 class="text-sm">2 - 0</h5>
+                        </div>
+                        <div class="paperPlane flex gap-3 items-center">
+                            <h5 class="text-sm">cookies</h5>
+                            <img src="/UTS/public/img/cookies.png" class="w-10 h-auto" alt="">
+                        </div>
+                    </div>
+                </div>
+                <hr class="border-[#2D2D2D]">
             </div>
         </div>
     </div>
