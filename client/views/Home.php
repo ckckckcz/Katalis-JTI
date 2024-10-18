0b2c02110e2e8c36bbbe857fdf41d1e2e152fd1d
ckckckcz <rriovld@gmail.com>
2024-10-18 23:10:25 +0700

    just update

--- client/views/Home.php
+++ client/views/Home.php
@@ -1,19 +1,19 @@
 <section class="flex justify-center sectionHome">
   <div class="py-8 px-4 text-center mx-auto max-w-screen-xl relative">
     <div class="flex -space-x-4 rtl:space-x-reverse items-left justify-left mb-4">
-      <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800 object-cover"
+      <img class="w-10 h-10 border-2 border-red-700 rounded-full object-cover"
         src="https://www.wwe.com/f/styles/gallery_img_l/public/all/2022/12/032_Roman_02102022PB_5570--6d0f152505dca2f8f9ae158c24210795.jpg"
         alt="User 1" />
-      <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800 object-cover"
+      <img class="w-10 h-10 border-2 border-red-700 rounded-full object-cover"
         src="https://www.wwe.com/f/styles/gallery_img_l/public/all/2022/04/001_WWE-ID-ATL__3543--904b22bc55d128feb73d42c15d626827.jpg"
         alt="User 2" />
-      <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800 object-cover"
+      <img class="w-10 h-10 border-2 border-red-700 rounded-full object-cover"
         src="https://www.wwe.com/f/styles/gallery_img_l/public/all/2016/11/037_RAW_01072013jg_0561_CS--e5c085d0289a433ffbdead5d88806cb8.jpg"
         alt="User 3" />
       <div class="flex items-center justify-center">
-        <a class="flex items-center justify-center w-10 h-10 text-xs font-bold text-white bg-red-600 border-2 border-white rounded-full hover:bg-red-600 dark:border-gray-800"
+        <a class="flex items-center justify-center w-10 h-10 text-xs font-bold text-white bg-red-600 border-2 border-red-700 rounded-full hover:bg-red-600 0"
           href="#"> +99 </a>
-        <span class="ml-2 text-sm font-bold text-white dark:text-gray-300 text-center">Pelanggan Puas</span>
+        <span class="ml-2 text-sm font-bold text-white text-center">Pelanggan Puas</span>
       </div>
     </div>
     <h1
