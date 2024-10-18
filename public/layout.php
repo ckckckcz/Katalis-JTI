c72e6bf64ac3ef49dbfff65f7008b403d2661b20
ckckckcz <rriovld@gmail.com>
2024-10-18 15:05:05 +0700

    update: Brand logo and position

--- public/layout.php
+++ public/layout.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo isset($title) ? $title : 'Grit & Grind'; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="/UTS/public/styles/global.css">
     <link rel="stylesheet" href="/UTS/public/styles/Navbar.css">
     <link rel="stylesheet" href="/UTS/public/styles/Home.css">
