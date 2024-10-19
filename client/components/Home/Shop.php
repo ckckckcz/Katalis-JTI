1c5ab460ebf12ce6cd01ea6aaee14278c16cf8b6
ckckckcz <rriovld@gmail.com>
2024-10-19 19:09:32 +0700

    update: responsive function

--- client/components/Home/Shop.php
+++ client/components/Home/Shop.php
@@ -1,10 +1,10 @@
 <section class="bg-white mt-20">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <div class="flex justify-center items-center">
+        <div class="lg:flex grid grid-cols-1 justify-center items-center">
             <img src="/UTS/public/img/shopNow.png" alt="">
-            <div class=" shopTitle p-5 text-5xl text-left">
-                <img src="/UTS/public/img/gritGrand.png" class="w-12 h-auto mb-4" alt="">
-                <h5 class="mb-6">top-rated quality merch</h5>
+            <div class="shopTitle p-1 mt-5 lg:mt-0 lg:p-5 text-5xl text-left">
+                <img src="/UTS/public/img/gritGrand.png" class="w-10 lg:w-12 h-auto mb-4" alt="">
+                <h5 class="mb-3 lg:mb-6 text-3xl lg:text-6xl">top-rated quality merch</h5>
                 <button type="button"
                     class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium text-sm px-7 py-3 me-2 mb-2 focus:outline-none">
                     Shop Now
