ea2ffa5510ac11e5617dd370d1909588c8bef433
ckckckcz <rriovld@gmail.com>
2024-10-22 11:32:25 +0700

    update: checkbox checked background

--- client/components/Premium/Form.php
+++ client/components/Premium/Form.php
@@ -42,14 +42,15 @@
             <input id="dropzone-file" type="file" class="hidden" />
         </label>
     </div>
-    <div class="flex items-start mb-5  mt-5">
+    <div class="flex items-start mb-5 mt-5">
         <div class="flex items-center h-5">
             <input id="terms" type="checkbox" value=""
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-red-300 ">
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-red-300 checked:bg-red-600">
         </div>
         <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a
                 href="#" class="text-red-600 hover:underline dark:text-red-500">terms and conditions</a></label>
     </div>
+
     <button type="submit"
         class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Register
         new account</button>
