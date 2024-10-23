e17bbf144fa81d5a5d06f986eadaaf2d422df9d7
ckckckcz <rriovld@gmail.com>
2024-10-23 10:49:00 +0700

    just update

--- client/components/Premium/Stepper/Step1.php
+++ client/components/Premium/Stepper/Step1.php
@@ -7,7 +7,7 @@
                 required />
         </div>
         <div class="mb-5">
-            <label for="games" class="block mb-2 text-sm font-medium text-gray-900">Selected Game</label>
+            <label for="games" class="block mb-2 text-sm font-medium text-gray-900">Select Game</label>
             <select id="games"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 required>
--- public/js/stepper.js
+++ public/js/stepper.js
@@ -46,7 +46,7 @@ document.getElementById("games").addEventListener("change", function () {
       // Add role input for each member
       memberRoleContainer.innerHTML += `
                 <div class="mb-5">
-                    <label for="role${i}" class="block mb-2 text-sm font-medium text-gray-900">Member ${i} Role</label>
+                    <label for="role${i}" class="block mb-2 text-sm font-medium text-gray-900"> Role ${i}</label>
                     <input type="text" id="role${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" required placeholder="Enter role for member ${i}">
                 </div>
             `;
