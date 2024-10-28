9392e34c1115d972f8521bca22effaa8b192f534
ckckckcz <rriovld@gmail.com>
2024-10-28 08:33:49 +0700

    update: Home page

--- client/components/Navbar.php
+++ client/components/Navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar">
     <div class="container">
+        <!-- Logo -->
         <a href="https://flowbite.com/" class="logo">
             <span class="logo-text">Katalis</span>
         </a>
-        <div class="actions">
-            <button type="button" class="button-primary">Get started</button>
-        </div>
+
+        <!-- Navbar Content (Links) -->
         <div class="navbar-content" id="navbar-cta">
             <ul class="navbar-list">
                 <li><a href="#" class="navbar-link active">Home</a></li>
@@ -13,6 +13,11 @@
                 <li><a href="#" class="navbar-link">Services</a></li>
                 <li><a href="#" class="navbar-link">Contact</a></li>
             </ul>
+        </div>
+
+        <!-- Actions (Button) -->
+        <div class="actions">
+            <button type="button" class="button-primary">Get started</button>
         </div>
     </div>
 </nav>
--- client/views/Home.php
+++ client/views/Home.php
@@ -1,31 +1,22 @@
-<section class="home-bg">
+<section class="home-section">
   <div class="home-container">
-    <div class="home-text-container">
-      <h1 class="home-title">We invest in the world’s potential</h1>
-      <p class="home-subtitle">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock
-        long-term value and drive economic growth.</p>
-      <div class="home-buttons">
-        <a href="#" class="home-button-primary">
-          Get started
-          <svg class="home-button-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9" />
-          </svg>
-        </a>
-        <a href="#" class="home-button-secondary">Learn more</a>
-      </div>
-    </div>
-    <div>
-      <iframe class="home-video" src="https://www.youtube.com/embed/KaLxCiilHns" title="YouTube video player"
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
+    <h1 class="home-title">We invest in the world’s potential</h1>
+    <p class="home-description">Here at Flowbite we focus on markets where technology, innovation, and capital can
+      unlock long-term value and drive economic growth.</p>
+    <div class="home-buttons">
+      <a href="#" class="home-button-primary">
+        Get started
+        <svg class="home-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9" />
+        </svg>
+      </a>
+      <a href="#" class="home-button-secondary">
+        Learn more
+      </a>
     </div>
   </div>
 </section>
 
+
 <?php include('./client/components/Home/Brand.php'); ?>
-<?php include('./client/components/Home/News.php'); ?>
-<?php include('./client/components/Home/Match.php'); ?>
-<?php include('./client/components/Home/Shop.php'); ?>
-<?php include('./client/components/Home/Community.php'); ?>
