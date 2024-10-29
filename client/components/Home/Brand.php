4ba557d998f259c5dae204f87cb51e65c64d9cdf
ckckckcz <rriovld@gmail.com>
2024-10-29 11:25:13 +0700

    update: brand logo image

--- client/components/Home/Brand.php
+++ client/components/Home/Brand.php
@@ -1,20 +1,16 @@
 <div class="page-logos">
     <section class="logos-grid">
         <div class="logo-item">
-            <img src="http://aic.turn.lv/content/themes/aic-theme/images/logos/logo1.jpg" alt="Logo 1"
-                class="logo-image">
+            <img src="/katalis/public/img/brands/polinema.png" alt="Polinema Logo" class="logo-image">
         </div>
         <div class="logo-item">
-            <img src="http://aic.turn.lv/content/themes/aic-theme/images/logos/logo2.jpg" alt="Logo 2"
-                class="logo-image">
+            <img src="/katalis/public/img/brands/jti.png" alt="JTI Logo" class="logo-image">
         </div>
         <div class="logo-item">
-            <img src="http://aic.turn.lv/content/themes/aic-theme/images/logos/logo3.jpg" alt="Logo 3"
-                class="logo-image">
+            <img src="/katalis/public/img/brands/wri.png" alt="WRI Logo" class="logo-image">
         </div>
         <div class="logo-item">
-            <img src="http://aic.turn.lv/content/themes/aic-theme/images/logos/logo4.jpg" alt="Logo 4"
-                class="logo-image">
+            <img src="/katalis/public/img/brands/hmti.png" alt="HMTI Logo" class="logo-image">
         </div>
     </section>
 </div>
