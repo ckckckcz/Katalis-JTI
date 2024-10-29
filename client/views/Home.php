3fcfc3150c899e05c63808559a6e6e5436987f56
ckckckcz <rriovld@gmail.com>
2024-10-29 15:53:17 +0700

    update: home section add umm some like tag

--- client/views/Home.php
+++ client/views/Home.php
@@ -1,9 +1,17 @@
 <section class="home-section">
   <div class="home-container">
+    <div class="home-tag font-bold">
+      JTIFast
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-fill"
+        viewBox="0 0 16 16">
+        <path
+          d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641z" />
+      </svg>
+    </div>
+
     <h1 class="home-title font-bold">Raih Prestasi, Wujudkan Mimpi 🏆</h1>
     <p class="home-description font-regular">Kami hadir untuk menginspirasi, mendukung, dan merayakan setiap pencapaian
-      serta langkah
-      maju anda dalam mencetak prestasi gemilang.</p>
+      serta langkah maju anda dalam mencetak prestasi gemilang.</p>
     <div class="home-buttons font-bold">
       <a href="#" class="home-button-primary">
         Get started
@@ -18,7 +26,6 @@
     </div>
   </div>
 </section>
-
 
 <?php include('./client/components/Home/Stats.php'); ?>
 <?php include('./client/components/Home/Blog.php'); ?>
