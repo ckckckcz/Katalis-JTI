5b23fe395909e276975be4b36f86a1ed1163ec34
ckckckcz <rriovld@gmail.com>
2024-11-01 09:27:23 +0700

    update: add icon at cards on Admi page

--- client/components/Admin/components/Cards.php
+++ client/components/Admin/components/Cards.php
@@ -1,20 +1,44 @@
 <div class="admin-card">
     <div href="#" class="custom-card">
-        <h5 class="custom-card-title font-bold">Noteworthy technology acquisitions 2021</h5>
-        <p class="custom-card-text font-regular">Here are the biggest enterprise technology acquisitions of 2021
-            so far, in
-            reverse chronological order.</p>
+        <div class="title-admin">
+            <h5 class="custom-card-title font-semi-bold">Jumlah Prestasi</h5>
+            <p class="custom-card-text font-bold">100+</p>
+        </div>
+        <div class="icon-admin">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                    clip-rule="evenodd" />
+            </svg>
+        </div>
+
     </div>
     <div href="#" class="custom-card">
-        <h5 class="custom-card-title font-bold">Noteworthy technology acquisitions 2021</h5>
-        <p class="custom-card-text font-regular">Here are the biggest enterprise technology acquisitions of 2021
-            so far, in
-            reverse chronological order.</p>
+        <div class="title-admin">
+            <h5 class="custom-card-title font-semi-bold">Jumlah Prestasi</h5>
+            <p class="custom-card-text font-bold">100+</p>
+        </div>
+        <div class="icon-admin">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                    clip-rule="evenodd" />
+            </svg>
+        </div>
+
     </div>
     <div href="#" class="custom-card">
-        <h5 class="custom-card-title font-bold">Noteworthy technology acquisitions 2021</h5>
-        <p class="custom-card-text font-regular">Here are the biggest enterprise technology acquisitions of 2021
-            so far, in
-            reverse chronological order.</p>
+        <div class="title-admin">
+            <h5 class="custom-card-title font-semi-bold">Jumlah Prestasi</h5>
+            <p class="custom-card-text font-bold">100+</p>
+        </div>
+        <div class="icon-admin">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                    clip-rule="evenodd" />
+            </svg>
+        </div>
+
     </div>
 </div>
--- client/components/Admin/components/Prestasi.php
+++ client/components/Admin/components/Prestasi.php
@@ -1,9 +1,5 @@
 <div class="prestasi-container">
         <div href="#" class="prestasi-card">
-                <h5 class="prestasi-card-title font-bold">Noteworthy technology acquisitions 2021</h5>
-                <p class="prestasi-card-text font-regular">Here are the biggest enterprise technology acquisitions of
-                        2021
-                        so far, in
-                        reverse chronological order.</p>
+                <h5 class="prestasi-card-title font-bold">Data Prestasi</h5>
         </div>
 </div>
