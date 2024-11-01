9571f761532a9a9b5c6ad0d04d5b01552afd3b37
ckckckcz <rriovld@gmail.com>
2024-11-01 07:06:11 +0700

    update: make components Header Admin

--- client/components/Admin/Sidebar.php
+++ client/components/Admin/Sidebar.php
@@ -1,28 +1,4 @@
-<nav class="sidebar-nav">
-    <div class="sidebar-container">
-        <div class="sidebar-flex sidebar-justify-between">
-            <div class="sidebar-flex sidebar-justify-start">
-                <!-- <button class="sidebar-button" onclick="toggleSidebar()">
-                    <span class="sidebar-sr-only">Open sidebar</span>
-                    <svg class="sidebar-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path clip-rule="evenodd" fill-rule="evenodd"
-                            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                        </path>
-                    </svg>
-                </button> -->
-                <div href="https://flowbite.com" class="sidebar-logo">
-                    <img src="/katalis/public/img/katalis.png" class="sidebar-logo-img" alt="Katalis Logo" />
-                    <span class="sidebar-logo-text font-bold">Katalis JTI</span>
-                </div>
-            </div>
-            <div class="sidebar-profile">
-                <img class="sidebar-profile-img" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="user photo">
-            </div>
-        </div>
-    </div>
-</nav>
-
+<?php include 'Header.php'; ?>
 <aside id="sidebar" class="sidebar-aside" aria-label="Sidebar">
     <div class="sidebar-menu">
         <ul class="sidebar-ul">
