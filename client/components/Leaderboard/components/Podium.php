a8f89ad4b1201334fcd2b09c823a75f8773eb3c8
ckckckcz <rriovld@gmail.com>
2024-11-25 13:12:07 +0700

    update: leaderboard style

--- client/components/Leaderboard/components/Podium.php
+++ client/components/Leaderboard/components/Podium.php
@@ -2,9 +2,18 @@
         <div class="podium-rank-container">
                 <h5 class="podium-card-title font-bold">Data Ranking</h5>
                 <div class="podium-grid">
-                        <div class="podium-cards podium-2 font-bold">Posisi - 2</div>
-                        <div class="podium-cards podium-1 font-bold">Posisi - 1</div>
-                        <div class="podium-cards podium-3 font-bold">Posisi - 3</div>
+                        <div class="podium-cards podium-2 font-bold">
+                                <img class="badge-silver badge-img" src="/katalis/public/img/badge/silver.png" alt="">
+                                Posisi - 2
+                        </div>
+                        <div class="podium-cards podium-1 font-bold">
+                                <img class="badge-gold badge-img" src="/katalis/public/img/badge/gold.png" alt="">
+                                Posisi - 1
+                        </div>
+                        <div class="podium-cards podium-3 font-bold">
+                                <img class="badge-bronze badge-img" src="/katalis/public/img/badge/bronze.png" alt="">
+                                Posisi - 3
+                        </div>
                 </div>
                 <div href="#" class="podium-card">
                         <div class="table-podium-container">
@@ -15,6 +24,7 @@
                                                         <th scope="col" class="table-podium-th">Mahasiswa</th>
                                                         <th scope="col" class="table-podium-th">Kompetisi</th>
                                                         <th scope="col" class="table-podium-th">Kategori</th>
+                                                        <th scope="col" class="table-podium-th">Poin</th>
                                                 </tr>
                                         </thead>
                                         <tbody class="font-regular">
@@ -25,6 +35,7 @@
                                                         <td class="table-podium-cell">Riovaldo Alfiyan Fahmi Rahman</td>
                                                         <td class="table-podium-cell">Compfest</td>
                                                         <td class="table-podium-cell">Nasional</td>
+                                                        <td class="table-podium-cell">1200</td>
                                                 </tr>
                                                 <tr class="table-podium-row">
                                                         <th scope="row"
@@ -33,6 +44,7 @@
                                                         <td class="table-podium-cell">Arya Mahendra Wijaya</td>
                                                         <td class="table-podium-cell">KMPIN</td>
                                                         <td class="table-podium-cell">Nasional</td>
+                                                        <td class="table-podium-cell">1100</td>
                                                 </tr>
                                                 <tr class="table-podium-row">
                                                         <th scope="row"
@@ -41,6 +53,7 @@
                                                         <td class="table-podium-cell">Bagaskara Pradipta Satriya</td>
                                                         <td class="table-podium-cell">Gemastik</td>
                                                         <td class="table-podium-cell">Nasioanl</td>
+                                                        <td class="table-podium-cell">1090</td>
                                                 </tr>
                                         </tbody>
                                 </table>
