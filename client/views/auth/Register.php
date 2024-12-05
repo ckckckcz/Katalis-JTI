4ac4d25f0e23c04be29798bbfd8b3e5928b0c85b
ckckckcz <rriovld@gmail.com>
2024-12-05 10:53:35 +0700

    update: page register and forgot password

--- client/views/auth/Register.php
+++ client/views/auth/Register.php
@@ -6,7 +6,8 @@
         <form class="login-form" method="POST" action="./server/proses/Login.php" onsubmit="return validateForm(event)">
             <div class="input-group nim">
                 <label for="text" class="login-label font-bold">NIM</label>
-                <input type="text" id="text" name="username" class="login-input font-semi-bold nimInput" placeholder="Masukkan nim anda">
+                <input type="text" id="text" name="username" class="login-input font-semi-bold nimInput"
+                    placeholder="Masukkan nim anda">
                 <span class="error-message font-semi-bold"></span>
             </div>
             <div class="input-group password">
@@ -14,12 +15,12 @@
                 <input type="password" id="password" name="password" class="login-input font-semi-bold"
                     placeholder="Masukkan kata sandi anda">
                 <span class="error-message font-semi-bold"></span>
-                <span class="forgot-container font-semi-bold pointer" onclick="window.location.href='/katalis/forgot'">Lupa Kata Sandi ?</span>
             </div>
             <button type="submit" class="login-button font-bold">Masuk</button>
             <div class="containerr font-bold">
                 <span class="login-help">
-                    <p class="login-help-1">Belum Punya Akun? <span onclick="window.location.href='/katalis/daftar'" class="login-solution">Daftar</span></p>
+                    <p class="login-help-1">Sudah Punya Akun? <span onclick="window.location.href='/katalis/login'"
+                            class="login-solution">Masuk</span></p>
                 </span>
             </div>
         </form>
